6a47436f891367c53fc4fe4676218ab9587ee966
Striderserge <sergio.barros@aluno.faculdadeimpacta.com.br>
2019-09-19 22:15:50 -0300

    mudado

--- com/Barros/test/test_teste.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from com.Barros.divisao import Teste 
-
-class Teste(TestCase):
-    def setUp(self):
-        self.Divisao = Divisao()
-    
-    def test_divisao(self):
-        self.assertEqual(self.Divisao.div_2(10),5,"deve ser 5")
